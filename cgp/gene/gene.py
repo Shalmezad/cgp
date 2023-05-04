ca4de4240af58d48577d20567d3b8f578b7aad37
Richard Wardin <shalmezad@gmail.com>
2023-05-04 19:13:22 -0400

    Sample problem

--- cgp/gene/gene.py
+++ cgp/gene/gene.py
@@ -6,7 +6,7 @@ import numpy.typing as npt
 
 @dataclass(frozen=True)
 class Gene:
-    NUM_OPS = 4
+    NUM_OPS = 14
 
     num_inputs: int
     middlenodes: tuple[int]
@@ -31,7 +31,6 @@ class Gene:
         in1 = self.evaluateNode(in1idx, input)
         in2 = self.evaluateNode(in2idx, input)
         in3 = self.evaluateNode(in3idx, input)
-        result = 0.0
         if op == 0:
             return np.absolute(in1)
         elif op == 1:
@@ -40,4 +39,24 @@ class Gene:
             return np.power(in1, 2)
         elif op == 3:
             return np.power(in1, 3)
-        return result
+        elif op == 4:
+            return (2 * np.exp(in1 + 1) - math.pow(math.e,2) - 1) / (math.pow(math.e,2) - 1)
+        elif op == 5:
+            return np.sin(in1)
+        elif op == 6:
+            return np.cos(in1)
+        elif op == 7:
+            return np.tanh(in1)
+        elif op == 8:
+            return in1 * -1
+        elif op == 9:
+            return np.where(in1 < 0.0, 0.0, 1.0)
+        elif op == 10:
+            return np.sqrt((np.power(in1, 2) + np.power(in2,2))/2.0)
+        elif op == 11:
+            return (in1 + in2) / 2.0
+        elif op == 12:
+            return (in1 - in2) / 2.0
+        elif op == 13:
+            return in1 * in2
+        raise ValueError("Unknown operator: {}".format(op))
