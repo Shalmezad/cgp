070802059489f42b4e45e6c59ad59cb4fcfa0767
Richard Wardin <shalmezad@gmail.com>
2023-05-22 19:44:51 -0400

    Improbed full experiment (maybe)

--- cgp/improbed/__init__.py
+++ cgp/improbed/__init__.py
@@ -1,5 +1,6 @@
 from .brain import Brain
 from .brain_builder import BrainBuilder
+from .brain_mutator import BrainMutator
 from .config import Config
 
-__all__ = ['Brain', 'BrainBuilder', 'Config']
+__all__ = ['Brain', 'BrainBuilder', 'BrainMutator', 'Config']
